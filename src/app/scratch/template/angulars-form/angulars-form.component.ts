eb51636482ae6184e6311d4b9d5dc8459d66a28e
krrahulnav <k.rahul@navriti.com>
2023-10-09 10:39:20 +0530

    initial change

--- src/app/scratch/template/angulars-form/angulars-form.component.ts
+++ src/app/scratch/template/angulars-form/angulars-form.component.ts
@@ -4,6 +4,7 @@ import { Router } from '@angular/router';
 import { GoogleSheetsDbService } from 'ng-google-sheets-db';
 import {GooglesheetService} from "../../../service/googlesheet.service";
 import { animate, query, stagger, state, style, transition, trigger } from '@angular/animations';
+import { interval } from 'rxjs';
 @Component({
   selector: 'app-angulars-form',
   templateUrl: './angulars-form.component.html',
@@ -46,7 +47,36 @@ export class AngularsFormComponent {
     private googleSheetsDbService: GoogleSheetsDbService, private googlesheet: GooglesheetService) {
     this.form();
   }
-  ngOnInit(): void {}
+  counter = 150;
+  interval$:any;
+
+  ngOnInit(): void {
+    this.startcounter();
+  }
+
+  startcounter(){
+    this.interval$ = interval(1000).subscribe(val=>{
+      this.counter--;
+      if(this.counter===0){
+        this.counter=150;
+        this.onSubmit();
+        this.startcounter();
+      }
+    });
+    setTimeout(()=>{
+      this.interval$.unsubscribe();
+    },1500000)
+  }
+  stopcounter(){
+    this.interval$.unsubscribe();
+    this.counter=0;
+  }
+  
+  getformatedtime(){
+    let mm = Math.floor(this.counter/60);
+    let ss = this.counter -mm *60;
+    return `${mm} min : ${ss} sec`;
+  }
   form(){
     this.forms = this.fb.group({
       fname:['',[Validators.required, Validators.pattern("^[a-zA-Z]*")] ],
@@ -71,9 +101,9 @@ export class AngularsFormComponent {
     return this.forms.controls;
   }
   onSubmit():void {
-     if (this.forms.valid) {
+     //if (this.forms.valid) {
        this.router.navigate(['/complete']);
-     }
+     //}
     this.googlesheet.saveFormData(this.forms.value).subscribe(console.log)
   }
   sheet(){
--- src/app/scratch/template/htcsjs/htcsjs.component.ts
+++ src/app/scratch/template/htcsjs/htcsjs.component.ts
@@ -4,6 +4,7 @@ import { Router } from '@angular/router';
 import { GoogleSheetsDbService } from 'ng-google-sheets-db';
 import { Googlesheetf2Service } from 'src/app/service/googlesheetf2.service';
 import { animate, query, stagger, state, style, transition, trigger } from '@angular/animations';
+import { interval } from 'rxjs';
 @Component({
   selector: 'app-htcsjs',
   templateUrl: './htcsjs.component.html',
@@ -46,7 +47,35 @@ export class HtcsjsComponent {
     private googlesheet: Googlesheetf2Service, private googleSheetsDbService: GoogleSheetsDbService) {
     this.form();
   }
-  ngOnInit(): void { }
+  counter = 150;
+  interval$:any;
+
+  ngOnInit(): void {
+    this.startcounter();
+   }
+
+  startcounter(){
+    this.interval$ = interval(1000).subscribe(val=>{
+      this.counter--;
+      if(this.counter===0){
+        this.counter=150;
+        this.onSubmit();
+        this.startcounter();
+      }
+    });
+    setTimeout(()=>{
+      this.interval$.unsubscribe();
+    },1500000)
+  }
+  stopcounter(){
+    this.interval$.unsubscribe();
+    this.counter=0;
+  }
+  getformatedtime(){
+    let mm = Math.floor(this.counter/60);
+    let ss = this.counter -mm *60;
+    return `${mm} min : ${ss} sec`;
+  }
   form() {
     this.forms = this.fb.group({
       fname: ['', [Validators.required, Validators.pattern("^[a-zA-Z]*")]],
@@ -72,10 +101,10 @@ export class HtcsjsComponent {
   }
   onSubmit(): void {
     //console.log(this.forms.value)
-    if (this.forms.valid) {
+    //if (this.forms.valid) {
       // Navigate to the next page using Angular Router
       this.router.navigate(['/complete']);
-    }
+    //}
     this.googlesheet.saveFormData(this.forms.value).subscribe(console.log)
     //alert("your form has been submitted");
 
