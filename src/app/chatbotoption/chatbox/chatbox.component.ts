2d62ed662c1a6287a52174c69efa9644f74080f3
krrahulnav <k.rahul@navriti.com>
2023-12-07 16:09:58 +0530

    all done

--- src/app/chatbotoption/chatbox/chatbox.component.ts
+++ src/app/chatbotoption/chatbox/chatbox.component.ts
@@ -13,8 +13,14 @@ export class ChatboxComponent {
   constructor(public chatboxs:ChatbotsService){}
 
   ngOnInit(): void {
-    const welcomeMessage: Message = { auther: 'bot', content: 'Hi! I am a chatbot. How can I help you?' };
+    const welcomeMessage: Message = { auther: 'bot', content: 'Hi' };
     this.messages.push(welcomeMessage);
+    
+    const welcom2: Message = { auther: 'bot', content: 'I am a chatbot.' };
+    this.messages.push(welcom2);
+    
+    const welcom3: Message = { auther: 'bot', content: 'How can I help you?' };
+    this.messages.push(welcom3);
 
     this.chatboxs.conversation.subscribe((val)=>{
       this.messages = this.messages.concat(val);      
--- src/app/service/chatbots.service.ts
+++ src/app/service/chatbots.service.ts
@@ -17,9 +17,29 @@ export class ChatbotsService {
   messagemap:any={
     "hello":"hi",
     "hi":"hello",
-    "how are you":"i am fine You",
+    "how are you":"Hello! I'm doing well, thank you for asking.",
     "who are you":"i am a chatbot. Who are you",
-    "default":"can't find the answer"
+    "Hello":"hi",
+    "Hi":"hello",
+    "How Are You":"Hello! I'm doing well, thank you for asking.",
+    "Who Are You":"i am a chatbot. Who are you",
+    "How are you":"Hello! I'm doing well, thank you for asking.",
+    "Who are you":"i am a chatbot. Who are you",
+    "Bye":"Goodbye! If you have more questions in the future, feel free to ask. Have a great day!",
+    "GoodBye":"Goodbye! If you have more questions in the future, feel free to ask. Have a great day!",
+    "Thank You":"Goodbye! If you have more questions in the future, feel free to ask. Have a great day!",
+    "bye":"Goodbye! If you have more questions in the future, feel free to ask. Have a great day!",
+    "goodbye":"Goodbye! If you have more questions in the future, feel free to ask. Have a great day!",
+    "thank you":"Goodbye! If you have more questions in the future, feel free to ask. Have a great day!",
+    "How Can I Create Account":"Click on the Login Button To create Account",
+    "how can i create account":"Click on the Login Button To create Account",
+    "Can I Create a Account Here":"Click on the Login Button To create Account",
+    "can i create a account here":"Click on the Login Button To create Account",
+    "How Can I Create A Form Without Login":"No You Can't Create A Form Without Login",
+    "how can i create a form without login":"No You Can't Create A Form Without Login",
+    "can i create a form after login":"Yes, You Can",
+    "Can I Create A Form After Login":"Yes, You Can",
+    "default":"can't recognized your text"
   }
 
   getbotanswer(msg:any){
